0c9be0aba53dc36b68ef20d6c0d3d9252b31a52d
Kees Jansen <kees@Keess-MacBook-Pro.local>
2023-10-11 14:49:27 +0100

    moved icons out of app component into main

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,6 @@ import Login from './components/pages/auth/Login';
 //Hooks
 import useToken from './hooks/useToken';
 
-// Icons
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { far } from '@fortawesome/free-regular-svg-icons';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-
-library.add(fas, far);
-
 // I could have created the ReactDOM root here, but keeping a more traditionla CRA style
 function App() {
   
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client'
 
 import './index.css'
 
+// Icons
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+library.add(fas, far);
+
 import App from './App.jsx'
 
 // I have kept a more traditional CRA style to keep things familiar
