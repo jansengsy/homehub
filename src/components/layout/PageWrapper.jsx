51ec0e435a8e8f310098f95dbfdea062eb070349
Kees Jansen <jansengsy@gmail.com>
2023-10-13 09:40:25 +0100

    fixed overflow issues with the page container

--- src/components/layout/PageWrapper.jsx
+++ src/components/layout/PageWrapper.jsx
@@ -1,6 +1,6 @@
 export default function PageWrapper({children}) {
   return (
-    <div className="bg-gray-800 m-0 w-full h-full p-4">
+    <div className="overflow-auto bg-gray-800 m-0 w-full h-full p-4">
       {children}
     </div>
   );
