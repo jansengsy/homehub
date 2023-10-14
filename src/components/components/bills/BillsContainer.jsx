340c505bec42cd5f5d334961bfe39bc92be9475f
Kees Jansen <jansengsy@gmail.com>
2023-10-14 13:28:56 +0100

    fixed width issue with bill container

--- src/components/components/bills/BillsContainer.jsx
+++ src/components/components/bills/BillsContainer.jsx
@@ -4,7 +4,7 @@ export default function BillsContainer({bills}) {
 
 
   return (
-    <div className='grid grid-cols-2 gap-4 max-h-72 overflow-y-scroll pr-4 my-2'>
+    <div className='grid grid-cols-2 gap-4 w-full max-h-72 overflow-y-scroll pr-4 my-2'>
       {bills.map((bill, index) => (
         <BillTile key={index} details={bill}/>
       ))}
