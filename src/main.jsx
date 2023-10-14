1da8e98c7c731e207f2c9344e06a622a5e24f299
Kees Jansen <jansengsy@gmail.com>
2023-10-14 12:40:59 +0100

    added the brands library from fa

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,9 @@ import './index.css'
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
-library.add(fas, far);
+library.add(fas, far, fab);
 
 import App from './App.jsx'
 
