c03a28b6700f13a4d282c868f5a418d5ab2fc8a8
Kees Jansen <jansengsy@gmail.com>
2023-10-15 15:41:04 +0100

    begun adding content to home page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,53 @@
-import { useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { AuthContext } from '../../context/AuthContext';
+
+import BillsContainer from '../components/bills/BillsContainer';
+import LoadingSpinner from '../components/LoadingSpinner';
 
 export default function Home() {
 
   const { user } = useContext(AuthContext);
 
+  const [bills, setBills] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('http://localhost:8080/bills', {
+      method: 'GET',
+    })
+		.then(response => response.json())
+		.then(data => {
+      setUpcomingBills(data);
+      setLoading(false);
+		})
+		.catch(error => {
+      console.error('Error fetching data:', error);
+		});
+  }, []);
+
+  const setUpcomingBills = (bills) => {
+
+    const recentBills = bills.filter((bill) => {
+      const today = new Date();
+      const due = new Date(bill.dueDate);
+
+      const timeDiff = due.getTime() - today.getTime();
+      const daysDiff = timeDiff / (1000 * 3600 * 24);
+
+      return daysDiff <= 7;
+    });
+
+    console.log(recentBills);
+    setBills(recentBills);
+  }
+
   return (
-    <h1 className='text-xl text-white pb-2'>Welcome back, {user.username}!</h1>
+    <>
+      <h1 className='text-xl text-white pb-2'>Welcome back, {user.username}!</h1>
+      <div>
+        <h1>Upcoming bills</h1>
+        { loading ? <LoadingSpinner /> : <BillsContainer bills={bills}/>}
+      </div>
+    </>
   );
 }
