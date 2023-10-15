ed0845c9a729ca6ac999f141a6ad0b7739e2df36
Kees Jansen <jansengsy@gmail.com>
2023-10-15 15:03:32 +0100

    changed the prop name in bill tile to be more descriptive of what it is

--- src/components/components/bills/BillTile.jsx
+++ src/components/components/bills/BillTile.jsx
@@ -1,8 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export default function BillTile({details}) {
+export default function BillTile({bill}) {
 
-  const { icon, heading, dueDate, amount } = details;
+  const { icon, heading, dueDate, amount } = bill;
 
   const formatDueDate = (date) => {
     const today = new Date();
