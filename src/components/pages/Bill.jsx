c8bb449c6221c5d13ccfb1e617f60eb4bc827650
Kees Jansen <jansengsy@gmail.com>
2023-10-15 17:23:48 +0100

    lintin fixes

--- src/components/pages/Bill.jsx
+++ src/components/pages/Bill.jsx
@@ -1,13 +1,13 @@
-import { Link, useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Bill() {
   return (
     <div>
       <Link to={'/bills/1'} >
-        <h1 className="text-white"><FontAwesomeIcon icon={'fa-solid fa-arrow-left'}/> Back to bills</h1>
+        <h1 className='text-white'><FontAwesomeIcon icon={'fa-solid fa-arrow-left'}/> Back to bills</h1>
       </Link>
-      <h1 className="text-2xl text-white">Bill page</h1>
+      <h1 className='text-2xl text-white'>Bill page</h1>
     </div>
   );
 }
