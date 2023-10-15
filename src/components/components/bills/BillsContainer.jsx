448895be59c39961f16709ee545d63867be062df
Kees Jansen <jansengsy@gmail.com>
2023-10-15 15:01:19 +0100

    updated billcontainer to use the card component to contain bill tiles

--- src/components/components/bills/BillsContainer.jsx
+++ src/components/components/bills/BillsContainer.jsx
@@ -1,16 +1,18 @@
 import BillTile from './BillTile';
 import Grid from '../../layout/Grid';
+import Card from '../../layout/Card';
 
 export default function BillsContainer({bills}) {
-
-
   return (
     <div className='max-h-72 overflow-scroll w-full'>
       <Grid>
         {bills.map((bill, index) => (
-          <BillTile key={index} details={bill}/>
+          <Card key={index} >
+            <BillTile bill={bill} />
+          </Card>
         ))}
       </Grid>
+      
     </div>
   );
 }
