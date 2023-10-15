f486863bbf3f6cea69a5dfcb5752962d0873e929
Kees Jansen <jansengsy@gmail.com>
2023-10-15 15:02:36 +0100

    updated styles in grid and card to be more flexible

--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -2,7 +2,7 @@ export default function Card({ children, click }) {
   return (
     <a
       onClick={click}
-      className='flex flex-col justify-between w-full h-64 p-2 shadow-md lg:max-w-lg bg-gray-700 text-white focus:outline-none hover:bg-gray-600 cursor-pointer '
+      className='w-full p-2 shadow-md lg:max-w-lg bg-gray-700 text-white focus:outline-none hover:bg-gray-600 cursor-pointer '
     >
       {children}
     </a>
