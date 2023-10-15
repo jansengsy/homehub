ba8fba735975b55449e991b63b7c4ed09e8dbc90
Kees Jansen <jansengsy@gmail.com>
2023-10-15 17:22:23 +0100

    removed button

--- src/components/pages/Bill.jsx
+++ src/components/pages/Bill.jsx
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function Bill() {
   return (
     <div>
-      <button onClick={() => navigate(-1)}>Go back!</button>
       <Link to={'/bills/1'} >
         <h1 className="text-white"><FontAwesomeIcon icon={'fa-solid fa-arrow-left'}/> Back to bills</h1>
       </Link>
